5e73f49b23fcd2b4dcb816f2608c98cbc0ba453b
coyo-tk <70998177+coyo-tk@users.noreply.github.com>
2020-09-17 11:42:18 +0300

    Add files via upload

---
+++ zadanie1.cpp
@@ -0,0 +1,38 @@
+﻿#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+
+using namespace std;
+
+double case2(double a, double b, double c, double d)
+{
+	return ((a / c) * (b / d)) - ((a * b - c) / (c * d));
+}
+
+double case4(double x, double y)
+{
+	return ((x + y) / (y + 1)) - ((x * y - 12) / (34 + x));
+}
+
+double case6(double x)
+{
+	return (x - ((pow(x, 3)) / 3) + ((pow(x, 5)) / 5));
+}
+
+double case10(double x, double y)
+{
+	return (pow((x + 1) / (x - 1), x)) + (18 * x * (pow(y, 2)));
+}
+
+int main()
+{
+	system("chcp 1251>nul");
+	int a, b, c, d, x, y;
+	cout << "Введите a,b,c,d";
+	cin >> a >> b >> c >> d;
+	cout << "Введите x,y";
+	cin >> x >> y;
+	cout << case2(a, b, c, d) << " " << case4(x, y) << " " << case6(x) << " " << case10(x, y) << " ";
+	return 0;
+}
